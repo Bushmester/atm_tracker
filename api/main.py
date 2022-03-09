315fd84a0ca3a7d3fdc53b4065fa1c88b883b5ff
Ramil Suleimanov <lllumineux@outlook.com>
2022-03-09 11:35:35 +0300

    Add basic logic

--- api/main.py
+++ api/main.py
@@ -1,8 +1,22 @@
+import asyncio
+import json
+
+import aiohttp
 from fastapi import FastAPI
+
+from constants import headers, body
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    response = None
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+                "https://api.tinkoff.ru/geo/withdraw/clusters",
+                headers=headers,
+                data=json.dumps(body)
+        ) as r:
+            response = await r.json()
+    return response
