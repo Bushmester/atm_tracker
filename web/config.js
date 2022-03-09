4660aa485d95c3102f9b3e3f7aa3f8d77348ff8d
Ilnar Gomelyanov <hibushland@gmail.com>
2022-03-09 20:30:00 +0300

    Add currencies and banks const

---
+++ web/config.js
@@ -0,0 +1,15 @@
+export const currencies = ["RUB", "USD", "EUR"];
+
+export const banks = {
+    "Tinkoff": "tcs",
+    "Sberbank": "11242",
+    "VTB Bank": "11249",
+    "Alfa-bank": "11250",
+    "Raiffeisen Bank": "11241",
+    "GazPromBank": "11371",
+    "MKB": "11475",
+    "Rosbank": "11248",
+    "Promsvyazbank": "11243",
+    "Uralsib": "11245",
+    "Otkritie": "11633"
+}
