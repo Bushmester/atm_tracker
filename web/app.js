eff9d7b0763186f28d2a596c0b2fe83eca7faaec
Ilnar Gomelyanov <hibushland@gmail.com>
2022-03-10 11:31:07 +0300

    Add async

--- web/app.js
+++ web/app.js
@@ -2,11 +2,11 @@ import {build_sidebar} from "./services/build_sidebar.js";
 import {get_data_from_sidebar} from "./services/get_data.js";
 
 
-window.onload = function (){
-    build_sidebar()
+window.onload = async function (){
+    await build_sidebar()
 
     const socket = new WebSocket("ws://localhost:8000/ws/{client}")
-    document.getElementById('search btn').onclick = () => {
-        socket.send(get_data_from_sidebar())
+    await document.getElementById('search btn').onclick = async () => {
+        await socket.send(get_data_from_sidebar())
     }
 }
