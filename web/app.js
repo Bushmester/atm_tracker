a2c35f3a4d5d2752d427b0cda2e7d7d84feaa54a
Ilnar Gomelyanov <hibushland@gmail.com>
2022-03-10 09:53:58 +0300

    Add event listener for search btn

--- web/app.js
+++ web/app.js
@@ -2,4 +2,6 @@ import {build_sidebar} from "./services/build_sidebar.js";
 
 window.onload = function (){
     build_sidebar()
+
+    document.getElementById('search btn').addEventListener('click', () => {})
 }
