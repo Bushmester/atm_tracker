f443e96e61f2d4f19caf7dde465294895e9523ee
Ilnar Gomelyanov <hibushland@gmail.com>
2022-03-10 08:08:30 +0300

    Fix build sidebar view

--- web/app.js
+++ web/app.js
@@ -1,24 +1,5 @@
-import currencies from "./config"
-import banks from "./config"
+import {build_sidebar} from "./services/build_sidebar.js";
 
-
-export function build_sidebar() {
-    let currency_temp = document.getElementById('currency-temp-id').content;
-
-    for (let currency of currencies) {
-        let copyHTML = document.importNode(currency_temp, true)
-        copyHTML.querySelector('input').setAttribute("value", currency)
-        copyHTML.querySelector('label').textContent = currency
-        document.getElementById("currencies").appendChild(copyHTML)
-    }
-
-    let bank_temp = document.getElementById('bank-temp-id').content;
-
-    for (let bank in banks) {
-        console.log(bank)
-        let copyHTML = document.importNode(bank_temp, true)
-        copyHTML.querySelector('input').setAttribute("value", banks[bank])
-        copyHTML.querySelector('label').textContent = bank
-        document.getElementById("banks").appendChild(copyHTML)
-    }
+window.onload = function (){
+    build_sidebar()
 }
