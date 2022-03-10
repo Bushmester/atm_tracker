0f66328cfd50aa1b7289ed6ccb60ce2f96b484c5
Ilnar Gomelyanov <hibushland@gmail.com>
2022-03-10 11:29:20 +0300

    Add user data send

--- web/app.js
+++ web/app.js
@@ -1,7 +1,12 @@
 import {build_sidebar} from "./services/build_sidebar.js";
+import {get_data_from_sidebar} from "./services/get_data.js";
+
 
 window.onload = function (){
     build_sidebar()
 
-    document.getElementById('search btn').onclick = () => {}
+    const socket = new WebSocket("ws://localhost:8000/ws/{client}")
+    document.getElementById('search btn').onclick = () => {
+        socket.send(get_data_from_sidebar())
+    }
 }
