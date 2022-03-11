45a80cd0caa67b40357f7d8dcfb1f965677aa497
Ramil Suleimanov <lllumineux@outlook.com>
2022-03-11 10:01:12 +0300

    Add states

--- bot/src/main.py
+++ bot/src/main.py
@@ -1,6 +1,7 @@
 import os
 
 from aiogram import Bot, Dispatcher, executor
+from aiogram.dispatcher.filters.state import StatesGroup, State
 from dotenv import find_dotenv, load_dotenv
 
 
@@ -12,6 +13,12 @@ API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+
+
+class MainSG(StatesGroup):
+    city = State()
+    currencies = State()
+    banks = State()
 
 
 if __name__ == '__main__':
