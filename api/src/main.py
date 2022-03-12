e75b709a713f6f37f074f8440c2993a35bb04192
Ilnar Gomelyanov <hibushland@gmail.com>
2022-03-12 11:44:38 +0300

    Clean root func

--- api/src/main.py
+++ api/src/main.py
@@ -1,24 +1,9 @@
-import asyncio
-import functools
-import json
-from typing import Callable
-
-import aiohttp
 from fastapi import FastAPI
 
-from config import HEADERS, BODY
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    response = None
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-                "https://api.tinkoff.ru/geo/withdraw/clusters",
-                headers=HEADERS,
-                data=json.dumps(BODY)
-        ) as r:
-            response = await r.json()
-    return response
+    pass
