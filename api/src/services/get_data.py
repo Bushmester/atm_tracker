9efbd93282218d6897b5c58e81c5180d5bb439ef
Ilnar Gomelyanov <hibushland@gmail.com>
2022-03-12 11:46:02 +0300

    Add get_data_about_atm_from_api

---
+++ api/src/services/get_data.py
@@ -0,0 +1,17 @@
+import json
+
+import aiohttp
+
+from src.config import HEADERS, BODY, API_FOR_ATM
+
+
+async def get_data_about_atm_from_api():
+    response = None
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+                API_FOR_ATM,
+                headers=HEADERS,
+                data=json.dumps(BODY)
+        ) as r:
+            response = await r.json()
+    return response
