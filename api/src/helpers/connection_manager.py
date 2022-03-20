23b060bb2e99523829d036d6faf86e0717132a1f
Ramil Suleimanov <lllumineux@outlook.com>
2022-03-20 18:37:36 +0300

    Update ConnectionManager.broadcast()

--- api/src/helpers/connection_manager.py
+++ api/src/helpers/connection_manager.py
@@ -39,25 +39,34 @@ class ConnectionManager(metaclass=Singleton):
             config = subs[sub]["config"]
             clients = subs[sub]["clients"]
             response = await get_data_about_atm_from_api(config["city"], config["currency"], config["banks"])
-            data = await serializer_response(response)
-
+            current_data = await serializer_response(response)
             data_hash = hash_data(config["city"], config["currency"], config["banks"])
             prev_data = self.subscribers.subscribers[data_hash].setdefault("data", {})
 
+            old_atms = {}
             new_atms = {}
             updated_atms = {}
             obsolete_atms = {}
-            for atm in data:
+
+            for atm in current_data:
                 if atm in prev_data:
-                    if prev_data[atm]["currencies"][config["currency"]] == data[atm]["currencies"][config["currency"]]:
-                        pass
+                    if prev_data[atm]["currencies"][config["currency"]] == current_data[atm]["currencies"][config["currency"]]:
+                        old_atms[atm] = current_data[atm]
                     else:
-                        updated_atms[atm] = data[atm]
+                        updated_atms[atm] = current_data[atm]
                 else:
-                    new_atms[atm] = data[atm]
+                    new_atms[atm] = current_data[atm]
+
+            for atm in prev_data:
+                if atm not in current_data:
+                    atm_data = prev_data[atm]
+                    atm_data["currencies"][config["currency"]] = 0
+                    obsolete_atms[atm] = atm_data
+
+            self.subscribers.subscribers[data_hash]["data"] = current_data
 
             for client in clients:
-                await client.send_bytes(json.dumps(data, ensure_ascii=False).encode())
+                await client.send_bytes(json.dumps(current_data, ensure_ascii=False).encode())
 
 
 manager = ConnectionManager()
