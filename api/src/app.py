2b3de916bc515da4fec61f2418551fd45ef40366
Ilnar Gomelyanov <hibushland@gmail.com>
2022-03-21 16:34:15 +0300

    Add repeat every for broadcast

--- api/src/app.py
+++ api/src/app.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi_utils.tasks import repeat_every
 
+from helpers.connection_manager import manager
 from view.websocket import router
 
 
 app = FastAPI()
 app.include_router(router)
+
+
+@app.on_event("startup")
+@repeat_every(seconds=60 * 5)
+async def make_broadcast():
+    if manager.subscribers.subscribers:
+        await manager.broadcast()
 
 
 @app.get("/")
